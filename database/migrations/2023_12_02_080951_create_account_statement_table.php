01118bc3384c4c0221e35ea37c56d967d6cc8857
NocDevX <xdev.noc@gmail.com>
2023-12-07 20:20:12 -0300

    07/12/23 - Ajuste na fk de acount_statement

--- database/migrations/2023_12_02_080951_create_account_statement_table.php
+++ database/migrations/2023_12_02_080951_create_account_statement_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('account_statement', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained();
             $table->double('closingBalance');
             $table->double('totalDebit');
             $table->double('totalCredit');
